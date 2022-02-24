f465610d8d8233888ebe711345c8baa79d2f147d
Odeke12 <trevodex@gmail.com>
2022-02-24 13:21:10 +0300

    New changes to server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Worlds!')
 })
 
 app.listen(port, () => {
