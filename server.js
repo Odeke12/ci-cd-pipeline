0bca02ffbcd804e46533bde221e8a2a2203fb115
Odeke12 <trevodex@gmail.com>
2022-02-24 22:26:05 +0300

    Second commit

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 const express = require('express')
 const app = express()
 require("dotenv").config();
-
+mongoose = require('mongoose')
 const port = process.env.PORT || 3000
+const studentRoutes = require('./routes/students')
+// const uri = "mongodb+srv://angulo:manuella@<your-cluster-url>/test?retryWrites=true&w=majority";
 
-app.get('/', (req, res) => {
-  res.send('Good job team')
-})
+mongoose.connect(
+    process.env.DATABASE,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // useCreateIndex: true,
+    },
+    (err) => {
+        if (err) throw err;
+        console.log('Connected');
+    }
+);
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.use('/students', studentRoutes);
+
+// app.get('/', (req, res) => {
+//   res.send('Good job team')
+// })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Listening on port ${port}`)
 })
